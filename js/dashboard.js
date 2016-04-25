b426019613a397a19e8cd47776d4751ce2b4495b
ericawarren <erwarren@gmail.com>
2016-04-24 23:28:35 -0400

    Adding simpel D3 bar chart

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,3 @@
-  /*
   var dataset;  // global variable for the data itself
   // d3.csv is async, so if other functions require the data to
   // be loaded, include them inside this function.
@@ -6,39 +5,54 @@
  d3.csv("data/counts.csv", function(error, data) {
     if(error) {   // if error is not NULL, i.e. data file loaded wrong
       console.log(error);
-    } else {
-      console.log(data);  // if file loaded correctly, go on with it
+    } else { // if file loaded correctly, go on with it
+      // width and height of main svg, in pixels
+      var w = 1000;
+      var h = 400;
+      var barPadding = 5;
 
       // Assign CSV data to variable so we can get it later
       dataset = data;
-      d3.select("main").selectAll("p")
+
+      // Create svg in <main> to bind data to
+      var svg = d3.select("main")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
+
+      // create svg elements and bind data to them
+      svg.selectAll("rect")
+         //Freq is frequency of each target
         .data(dataset)
         .enter()
-        .append("p")
-        .text(function(d) { return d; })
-        .style("color", "red");
+        .append("rect")
+        .attr("x", function(d, i) {
+          return i * (w / dataset.length);
+        })
+        .attr("width", w / dataset.length - barPadding)
+        .attr("y", function(d) {
+          return h - +d["Freq"]/100;
+        })
+        .attr("height", function(d) {
+          return +d["Freq"]/100;
+        })
+        .attr("class", function(d) {
+          return d["Var1"];
+        });
+
+        svg.selectAll("text")
+           .data(dataset)
+           .enter()
+           .append("text")
+           .text(function(d) {
+              return d["Var1"];
+           })
+           .attr("x", function(d, i) {
+              return i * (w / dataset.length) + 15;
+           })
+           .attr("y", function(d) {
+              return h - (+d["Freq"]/100) + 30;
+           })
+           .attr("fill", "white");
     }
   });
-  */
-  // width and height of main svg, in pixels
-  var w = 500
-  var h = 50
-
-  // something simplier to play with
-  var svg = d3.select("main")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
-  var dataset = [ 5, 10 ,15 , 20, 25 ]
-  var circles = svg.selectAll("circle")
-                  .data(dataset)
-                  .enter()
-                  .append("circle");
-  circles.attr("cx", function(d, i) {
-    return (i * 50) + 25;
-  })
-    .attr("cy", h/2)
-    .attr("r", function(d) {
-      return d;
-    });
